4250839ee932b8b7edace04802b1aa57faae62ec
Firas <firasmumtaz12@gmail.com>
2023-09-14 02:15:21 +0530

    route model binding

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/posts/{post}', function ($id) {
+Route::get('/posts/{post:slug}', function (Post $post) {
     //Find a post by its slug and pass it to a view called "post"
 
     return view('post', [
-        'post' => Post::find($id)
+        'post' => $post
     ]);
 
 
