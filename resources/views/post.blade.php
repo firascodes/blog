27393dc8a658e77158ea0f0c953f3099ecd37a34
Firas <firasmumtaz12@gmail.com>
2023-09-14 02:15:39 +0530

    added categories table and displayed it

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,12 @@
 <x-layout>
 
     <article>
-        <h1>{{$post->title}}</h1>
+        <h1>{{ $post->title }}</h1>
         <!-- We are recieving the post variable from route 
          'post' => $post_path -->
-        <div>
-            {!! $post->body !!}
-        </div </article>
+        {!! $post->body !!}
+    </article>
 
-        <a href="/">Go Back</a>
+    <a href="/">Go Back</a>
 
 </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,8 +3,14 @@
 
     <article>
         <h1>
-            <a href="/posts/{{$post->id}}"> {{$post->title}} </a>
+            <a href="/posts/{{$post->slug}}">
+                {{$post->title}} </a>
         </h1>
+
+        <p>
+            <a href="#">{{ $post->category->name}}</a>
+        </p>
+
         <div>
             {{$post->excerpt}}
         </div>
