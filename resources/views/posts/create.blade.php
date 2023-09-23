ffd5e0b004fc73752dfbb38dd64c6ca1c1adb083
Firas <firasmumtaz12@gmail.com>
2023-09-23 21:54:52 +0530

    added post create form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,24 +1,50 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <div class="border border-gray-200 p-6 rounded-xl max-w-sm mx-auto">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">Publish New Post!</h1>
+        <div class="border border-gray-200 p-6 rounded-xl ">
             <form action="/admin/posts" method="POST">
                 @csrf
                 <div class="mb-6">
                     <label for="title" class="block mb-2 uppercase font-bold ">
                         Title
                     </label>
-                    <input type="text" name="title" id="title" class="text-black border border-gray-800 p-2 w-full">
+                    <input type="text" name="title" id="title" value="{{old('title')}}"
+                        class="text-black border border-gray-800 p-2 w-full">
                     @error('title')
                     <p class="text-red-500 text-xs mt-2">{{$message}}</p>
                     @enderror
 
                 </div>
                 <div class="mb-6">
+                    <label for="slug" class="block mb-2 uppercase font-bold ">
+                        slug
+                    </label>
+                    <input type="text" name="slug" id="slug" value="{{old('slug')}}"
+                        class="text-black border border-gray-800 p-2 w-full">
+                    @error('slug')
+                    <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                    @enderror
+
+                </div>
+
+                <div class="mb-6">
+                    <label for="thumbnail" class="block mb-2 uppercase font-bold ">
+                        thumbnail
+                    </label>
+                    <input type="file" name="thumbnail" id="thumbnail" value="{{old('thumbnail')}}"
+                        class="text-black border border-gray-800 p-2 w-full bg-white">
+                    @error('thumbnail')
+                    <p class="text-red-500 text-xs mt-2">{{$message}}</p>
+                    @enderror
+
+                </div>
+
+                <div class="mb-6">
                     <label for="excerpt" class="block mb-2 uppercase font-bold ">
                         Excerpt
                     </label>
                     <textarea type="text" name="excerpt" id="excerpt"
-                        class="text-black border border-gray-800 p-2 w-full"></textarea>
+                        class="text-black border border-gray-800 p-2 w-full">{{old('excerpt')}}</textarea>
                     @error('excerpt')
                     <p class="text-red-500 text-xs mt-2">{{$message}}</p>
                     @enderror
@@ -29,7 +55,7 @@
                         Body
                     </label>
                     <textarea type="text" name="body" id="body"
-                        class="text-black border border-gray-800 p-2 w-full"></textarea>
+                        class="text-black border border-gray-800 p-2 w-full">{{old('body')}}</textarea>
                     @error('body')
                     <p class="text-red-500 text-xs mt-2">{{$message}}</p>
                     @enderror
@@ -47,7 +73,8 @@
                         @endphp
 
                         @foreach ($categories as $category)
-                        <option value="{{$category->id}}">{{ucwords($category->name)}}</option>
+                        <option value="{{$category->id}}" {{old('category_id')==$category->id ? 'selected' :
+                            ''}}>{{ucwords($category->name)}}</option>
                         @endforeach
 
                     </select>
