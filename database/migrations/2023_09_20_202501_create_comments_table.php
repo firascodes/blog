b25f093f956a67ba946de1460d3c49ba6ed0fad5
Firas <firasmumtaz12@gmail.com>
2023-10-01 21:21:34 +0530

    foreign id fix

--- database/migrations/2023_09_20_202501_create_comments_table.php
+++ database/migrations/2023_09_20_202501_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
         });
